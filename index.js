91dcf2d73abaf283b295634a8cff7f4c86807deb
Santeri Saariokari <santeri.saariokari@tuni.fi>
2019-07-01 22:52:42 +0300

    puhelinluettelon backend step6

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+    },
+    { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+    },
+    { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+    },
+    { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
+    }
+]
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(persons)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }else if(persons.filter(obj => {
+    return obj.name === body.name}).length != 0
+  ){
+    return response.status(400).json({
+        error: 'name must be unique'
+      })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000000)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people
+                   </br></br> ${new Date()}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
