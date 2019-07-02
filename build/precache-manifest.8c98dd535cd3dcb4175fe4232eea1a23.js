970f4ede5881aebc3f9f5a2b62c1c6889aece13e
Santeri Saariokari <santeri.saariokari@tuni.fi>
2019-07-02 21:13:24 +0300

    uibuild

---
+++ build/precache-manifest.8c98dd535cd3dcb4175fe4232eea1a23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3a5c274f59b02c9ed16f0d3b92a0f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dfe036308adae9ee467",
+    "url": "/static/css/main.06f1e1b8.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "8dfe036308adae9ee467",
+    "url": "/static/js/main.b6864610.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
