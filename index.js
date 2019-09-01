40d3ca3ee8754c6b80b809f66d39f71c128f7a62
Santeri Saariokari <santeri.saariokari@tuni.fi>
2019-09-01 13:56:50 +0300

    3.16: puhelinluettelo ja tietokanta, step3

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 app.use(cors())
 app.use(express.static('build'))
 
-
-
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -41,25 +39,31 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons.map(person => person.toJSON()))
-  });
+  Person.find({})
+    .then(persons => {
+      response.json(persons.map(person => person.toJSON()))
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people
+    response
+      .send(`Phonebook has info for ${persons.length} people
                    </br></br> ${new Date()}`)
+      .catch(error => next(error))
+
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
   .then(person => {
-    response.json(person.toJSON())
+    if (person) {
+      response.json(person.toJSON())
+    } else {
+      response.status(204).end() 
+    }
   })
-  .catch(error => {
-    console.log(error);
-    response.status(404).end()
-  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -69,6 +73,28 @@ app.delete('/api/persons/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
+
+  next(error)
+}
+
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
