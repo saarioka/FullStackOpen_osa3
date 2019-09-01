a855ce3d06e5fdef1024fbe88cf49ebc597405c5
Santeri Saariokari <santeri.saariokari@tuni.fi>
2019-09-01 13:23:14 +0300

    3.13: puhelinluettelo ja tietokanta, step1 & 2

--- index.js
+++ index.js
@@ -1,69 +1,49 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-
 const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const Person = require('./models/person')
 
+app.use(bodyParser.json())
 morgan.token('data', request => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-
 app.use(cors())
-
 app.use(express.static('build'))
 
-let persons = [
-    { 
-    "name": "Arto Hellas", 
-    "number": "040-123456",
-    "id": 1
-    },
-    { 
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
-    "id": 2
-    },
-    { 
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
-    "id": 3
-    },
-    { 
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
-    "id": 4
-    }
-]
+
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
+  /*if (!body.name || !body.number) {
+    console.log('content missing')
     return response.status(400).json({
       error: 'content missing'
     })
-  }else if(persons.filter(obj => {
-    return obj.name === body.name}).length != 0
-  ){
+  }else if(Person.findById({name: body.name}).count != 0){
+    console.log('content missing')
     return response.status(400).json({
         error: 'name must be unique'
       })
   }
-
-  const person = {
+*/
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 1000000)
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
+  })
+  console.log("123");
+  person.save().then(response => {
+  console.log(`Added ${body.name} number ${body.number} to phonebook`);
+  })
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons.map(person => person.toJSON()))
+  });
 })
 
 app.get('/info', (request, response) => {
@@ -72,13 +52,16 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  //const id = Number(request.params.id)
+  //const person = persons.find(person => person.id === id)
+  //if (person) {
+  //  response.json(person)
+  //} else {
+  //  response.status(404).end()
+  //}
+  Person.findById(request.params.id).then(person => {
+    response.json(person.toJSON())
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
